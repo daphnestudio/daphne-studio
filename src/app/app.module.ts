111648c81c17f4021a7c01a78e0e2317f04274d6
Manuela Scotto <manuelascotto18@gmail.com>
2020-06-09 17:59:03 +0200

    footer desktop and mobile added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {APP_ROUTES} from './app.route';
+import { APP_ROUTES } from './app.route';
 import { NavbarModule } from './control/navbar/navbar.module';
 import { LoaderModule } from './control/loader/loader.module';
 import { EventManagerService } from './service/base/event-manager.service';
+import { FooterModule } from './control/footer/footer.module';
 
 @NgModule({
   declarations: [
@@ -15,9 +16,10 @@ import { EventManagerService } from './service/base/event-manager.service';
     BrowserModule,
     APP_ROUTES,
     NavbarModule,
+    FooterModule
     // LoaderModule,
   ],
-    providers: [EventManagerService],
+  providers: [EventManagerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
