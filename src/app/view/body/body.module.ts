3a9c8cfef269061083e01f676643cdea86d7dff8
Gloria <g.giannascoli@libero.it>
2020-06-09 21:16:02 +0200

    form styled :)

--- src/app/view/body/body.module.ts
+++ src/app/view/body/body.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 import { BodyComponent } from './body.component';
 import { BODY_ROUTES } from './body-routing.module';
 import { BackendService } from 'src/app/service/backend-caller.service';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [BodyComponent],
   imports: [
     CommonModule,
     BODY_ROUTES,
-  ], 
+    ReactiveFormsModule,
+    FormsModule
+  ],
   providers: [BackendService]
 })
 export class BodyModule { }
