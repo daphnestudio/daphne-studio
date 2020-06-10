80a92478d8741fee79f87d89bd67f706059657d4
Yasser <yasser1998@outlook.it>
2020-06-10 02:39:13 +0200

    body form added with validation

--- src/app/view/body/body.component.ts
+++ src/app/view/body/body.component.ts
@@ -1,22 +1,42 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { BaseComponent } from 'src/app/util/base-view';
-import { EventManagerService } from 'src/app/service/base/event-manager.service';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { BaseComponent } from "src/app/util/base-view";
+import { EventManagerService } from "src/app/service/base/event-manager.service";
+import { FormGroup, FormBuilder, Validators, FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'daphne-body',
-  templateUrl: './body.component.html',
-  styleUrls: ['./body.component.scss'],
-  encapsulation: ViewEncapsulation.None,
+	selector: "daphne-body",
+	templateUrl: "./body.component.html",
+	styleUrls: ["./body.component.scss"],
+	encapsulation: ViewEncapsulation.None,
 })
 export class BodyComponent extends BaseComponent<any> implements OnInit {
 
-  mainBodyImage: string ='https://it.jobrapido.com/blog/wp-content/uploads/sites/2/2018/06/Beauty-Consultant-di-cosa-si-occupa.jpg';
+	mainBodyImage: string = "https://it.jobrapido.com/blog/wp-content/uploads/sites/2/2018/06/Beauty-Consultant-di-cosa-si-occupa.jpg";
+	bodyformGroup: FormGroup;
+	whatsappMessage: string = "";
 
-  constructor(eventManager: EventManagerService) {
-    super(eventManager);
-  }
-  ngOnInit() {
-    console.log(this.isOnMobile)
-  }
+	constructor(eventManager: EventManagerService, private fb: FormBuilder) {
+		super(eventManager);
+		this.createForm();
+	}
 
+	ngOnInit() { }
+
+	createForm() {
+		this.bodyformGroup = this.fb.group({
+			name: new FormControl("", Validators.required),
+			surname: new FormControl("", Validators.required),
+			birthDate: new FormControl("", Validators.required),
+			email: new FormControl("", [
+				Validators.required,
+				Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$"),
+			]),
+		});
+	}
+
+	sendFormData() {
+		this.whatsappMessage = `Nome: *${this.bodyformGroup.value.name}* Cognome: *${this.bodyformGroup.value.surname}* Data di nascita: *${this.bodyformGroup.value.birthDate}*. E-mail: *${this.bodyformGroup.value.email}*`;
+		let url = `https://wa.me/00393275360115/?text=${this.whatsappMessage}`;
+		window.open(url, "_blank");
+	}
 }
--- src/app/view/body/body.module.ts
+++ src/app/view/body/body.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { BodyComponent } from './body.component';
 import { BODY_ROUTES } from './body-routing.module';
 import { BackendService } from 'src/app/service/backend-caller.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [BodyComponent],
   imports: [
     CommonModule,
     BODY_ROUTES,
-  ], 
+    ReactiveFormsModule
+  ],
   providers: [BackendService]
 })
 export class BodyModule { }
