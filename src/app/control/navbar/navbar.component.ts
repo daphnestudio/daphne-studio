7aa5a9b34b070551ce7f5d1d7c9f3024b8855e47
Yasser <yasser1998@outlook.it>
2020-06-11 03:23:32 +0200

    new mobile navabr + new fonts and some graphic fixes

--- src/app/control/navbar/navbar.component.ts
+++ src/app/control/navbar/navbar.component.ts
@@ -18,46 +18,26 @@ declare var window: any;
   encapsulation: ViewEncapsulation.None,
 })
 export class NavbarComponent extends BaseComponent<any> implements OnInit {
-  currentUrl: string = "";
-  isService: boolean = false;
-  isWorkWithUs: boolean = false;
-  isContacts: boolean = false;
-  isFeedbacks: boolean = false;
+  currentUrl: Object = {};
   isScrolled: number = 0;
   constructor(router: Router, eventManager: EventManagerService) {
     super(eventManager);
-    // TODO: che schifo, da cambiare assolutamente !!!
     router.events.subscribe((res) => {
-      this.currentUrl = router.url;
-      if (this.currentUrl.includes("/servizi")) {
-        this.isService = true;
-        this.isWorkWithUs = false;
-        this.isContacts = false;
-        this.isFeedbacks = false;
-      } else if (this.currentUrl === "/dicono-di-noi") {
-        this.isFeedbacks = true;
-        this.isService = false;
-        this.isWorkWithUs = false;
-        this.isContacts = false;
-      } else if (this.currentUrl === "/lavora-con-noi") {
-        this.isFeedbacks = false;
-        this.isService = false;
-        this.isWorkWithUs = true;
-        this.isContacts = false;
-      } else if (this.currentUrl === "/contatti") {
-        this.isFeedbacks = false;
-        this.isService = false;
-        this.isWorkWithUs = false;
-        this.isContacts = true;
-      }
+      this.currentUrl = this.getCurrentUrl(router.url);
+      ;
     });
   }
 
   ngOnInit() {
-    $(function () {
-      var navMain = $(".navbar-collapse");
-      navMain.on("click", "a", null, function () {
-        navMain.collapse("hide");
+    $(document).ready(function () {
+      $(".open-menu").on("click", function () {
+        $(".overlay").addClass("open");
+      });
+      $(".close-menu").on("click", function () {
+        $(".overlay").removeClass("open");
+      });
+      $("a").on("click", function () {
+        $(".overlay").removeClass("open");
       });
     });
   }
